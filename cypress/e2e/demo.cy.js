b899d55f207cd8c86696d3ab75ebd6fa054bcd4d
El Moussaoui <khalid.el-moussaoui@sogeti.com>
2024-10-08 10:40:22 +0200

    Second Cypress Test, Assert Title

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -4,6 +4,12 @@ describe('Cypress E2E Testing Demo', () => {
   it('Assert Url', () => {
     cy.visit('https://example.com/')
     cy.url().should('contain', 'example.com')
-     
+  })
+
+  it('Assert Title', () => {
+    cy.visit('https://example.com')
+    cy.title().should('contain', 'Example Domain')
+    
+
   })
 })
