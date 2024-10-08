f54f1658f24cf683b7b24de4ca67d773531a4162
El Moussaoui <khalid.el-moussaoui@sogeti.com>
2024-10-08 12:11:44 +0200

    beforeEach, afterEach, only, skip function in Cypress

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -1,25 +1,30 @@
 /// <reference types="Cypress" />
 
 describe('Cypress E2E Testing Demo', () => {
-  it('Assert Url', () => {
+
+  beforeEach(() => {
     cy.visit('https://example.com/')
+  })
+
+  afterEach(() => {
+    cy.log('Running after each of my tests')
+  })
+
+  it('Assert Url', () => {
     cy.url().should('contain', 'example.com')
   })
 
   it('Assert Title', () => {
-    cy.visit('https://example.com')
     cy.title().should('contain', 'Example Domain')
   })
 
   it('Assert Element', () => {
-    cy.visit('https://example.com')
     cy.get('h1').should('be.visible')
     cy.wait(2000)
     cy.get('p').should('be.visible')
   })
 
   it('Reload and Logs', () => {
-    cy.visit('https://example.com')
     cy.log('Before Reload')
     cy.reload()
     cy.log('After Reload')
