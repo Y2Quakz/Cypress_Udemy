96da9715c879d8001875176de740672864866d02
El Moussaoui <khalid.el-moussaoui@sogeti.com>
2024-10-08 11:50:58 +0200

    Reload and Logs Cypress

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -17,4 +17,12 @@ describe('Cypress E2E Testing Demo', () => {
     cy.wait(2000)
     cy.get('p').should('be.visible')
   })
+
+  it('Reload and Logs', () => {
+    cy.visit('https://example.com')
+    cy.log('Before Reload')
+    cy.reload()
+    cy.log('After Reload')
+
+  })
 })
