3356c7dfa17fcd31881fd80413fc9643fa50119e
El Moussaoui <khalid.el-moussaoui@sogeti.com>
2024-10-13 14:02:42 +0200

    Forms, Inputs & Buttons Part 2

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,16 +1,40 @@
 describe('Gorms, Buttons & Checkbox', () => {
   it('Submit And Assert Form', () => {
+    
+    //Variabmes with Data
+    const name = 'Kay'
+    const drink = 'Water'
+    const color = 'Red'
+    const email = 'kay#test.com'
+    const message = 'I Like Cypress'
+
+
+
     // Load the Form
     cy.visit('https://practice-automation.com/form-fields/')
+    
+    //OutDated Site don't have this class anymore.
     //cy.get('[data-test="contact-form"]').should('be.visible')
+    cy.get('.entry-content').should('be.visible')
+
 
     // Fill the Form
-    cy.get('#name-input').type('Peter')
-    cy.contains('Water').click()
-    cy.contains('Red').click()
-    cy.get('#email').type('peter@test.com')
-    cy.get('#message').type('I Like Cypress')
+    cy.get('#name-input').type(name)
+    cy.contains(drink).click()
+    cy.contains(color).click()
+    cy.get('#email').type(email)
+    cy.get('#message').type(message)
     cy.get('#submit-btn').click()
-    // Assert Form Details
+
+
+   // Assert Form Details // OutDated Site don't redirect, just leave a message 
+   // cy.get('.contact-form-submission').should('be.visible')
+   // cy.get('.field-value').contains(name).should('be.visible')
+   // cy.get('.field-value').contains(drink).should('be.visible')
+   // cy.get('.field-value').contains(color).should('be.visible')
+   // cy.get('.field-value').contains(email).should('be.visible')
+   // cy.get('.field-value').contains(message).should('be.visible')
+
+
   })
 })
