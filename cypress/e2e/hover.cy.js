9c1c03ce801f03ca312a899a7744316768fd11f0
El Moussaoui <khalid.el-moussaoui@sogeti.com>
2024-10-13 22:32:09 +0200

    Device Emulation and Viewport

--- cypress/e2e/hover.cy.js
+++ cypress/e2e/hover.cy.js
@@ -3,6 +3,8 @@ const URLsite = 'https://practice-automation.com/hover/'
 
 describe('Cypress Hover', () => {
   it('Lets test hover workaround', () => {
+    //cy.viewport(800, 600)
+    cy.viewport('macbook-15')
     cy.visit(URLsite)
     cy.get('#mouse_over').trigger('mouseover')
     cy.get('#mouse_over').should('contain', 'You did it!')
