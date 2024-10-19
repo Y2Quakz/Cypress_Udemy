5c89264fda7534d1cf0444c4b6443061fbf8ddb1
El Moussaoui <khalid.el-moussaoui@sogeti.com>
2024-10-19 11:38:07 +0200

    Custom Commands

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -17,25 +17,34 @@ describe('Forms, Buttons & Checkbox', () => {
     
     //OutDated Site don't have this class anymore.
     //cy.get('[data-test="contact-form"]').should('be.visible')
-    cy.get('.entry-content').should('be.visible')
-
+    //cy.get('.entry-content').should('be.visible')
+    cy.getByClass('entry-content').should('be.visible')
 
     // Fill the Form
-    cy.get('#name-input').type(name)
+    //cy.get('#name-input').type(name)
+    cy.getById('name-input').type(name)
     cy.contains(drink).click()
     cy.contains(color).click()
-    cy.get('#email').type(email)
-    cy.get('#message').type(message)
-    cy.get('#submit-btn').click()
-
+    //cy.get('#email').type(email)
+    cy.getById('email').type(email)
+    //cy.get('#message').type(message)
+    cy.getById('message').type(message)
+    //cy.get('#submit-btn').click()
+    cy.getById('submit-btn').click()
 
    // Assert Form Details // OutDated Site don't redirect, just leave a message 
-   // cy.get('.contact-form-submission').should('be.visible')
+    //cy.get('.contact-form-submission').should('be.visible')
+      //cy.getByClass('contact-form-submission').contains(name).should('be.visible')
    // cy.get('.field-value').contains(name).should('be.visible')
+      //cy.getByClass('field-value').contains(name).should('be.visible')
    // cy.get('.field-value').contains(drink).should('be.visible')
+      //cy.getByClass('field-value').contains(drink).should('be.visible')
    // cy.get('.field-value').contains(color).should('be.visible')
+      //cy.getByClass('field-value').contains(color).should('be.visible')
    // cy.get('.field-value').contains(email).should('be.visible')
+      //cy.getByClass('field-value').contains(email).should
    // cy.get('.field-value').contains(message).should('be.visible')
+      //cy.clickByClass('field-value').contains(message).should('be.visible')
 
 
   })
