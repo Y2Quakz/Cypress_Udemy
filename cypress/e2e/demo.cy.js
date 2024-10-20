6a168672c679e1496e77b6a1658f2d6424da3094
El Moussaoui <khalid.el-moussaoui@sogeti.com>
2024-10-20 21:54:32 +0200

    Test Retries

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -20,9 +20,10 @@ describe('Cypress E2E Testing Demo', () => {
     examplePage.assertTitle()
   })
 
-  it.only('Assert Element', { retries: 3}, () => {
+  it.only('Assert Element', { retries: 4}, () => {
     
-   // cy.get('h1').should('not.be.visible') //Retries : 3
+
+   // cy.get('h1').should('not.be.visible') //Retries : 3 
     cy.get('h1').should('be.visible')
     cy.wait(2000)
     cy.get('p').should('be.visible')
