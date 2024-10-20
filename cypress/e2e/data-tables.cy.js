a9a5c8bb977eaccfdc3f1295a58eb25fdea8e140
El Moussaoui <khalid.el-moussaoui@sogeti.com>
2024-10-20 21:18:55 +0200

    Data Tables

---
+++ cypress/e2e/data-tables.cy.js
@@ -0,0 +1,11 @@
+
+
+describe('Data Tables', () => {
+  it('Validate items inside data table', () => {
+    cy.visit('https://practice-automation.com/tables/')
+    cy.getByClass('wp-block-table').within(() => {
+      cy.get('td').eq(0).should('contain', 'Item')
+      cy.get('td').eq(1).should('contain', 'Price')
+    })
+  })
+})
