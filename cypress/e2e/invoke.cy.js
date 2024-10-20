14ddc8cb6ff9870e22ed13dcc0848f13a5d394ea
El Moussaoui <khalid.el-moussaoui@sogeti.com>
2024-10-20 18:45:45 +0200

    Clearing Invoke Function

---
+++ cypress/e2e/invoke.cy.js
@@ -0,0 +1,10 @@
+
+describe('Execute javascript code inside cypress', () => {
+  it('Invoke calculation of numbers', () => {
+    const fn = (a, b, c) => {
+      return a + b + c;
+    }
+    cy.wrap({sum: fn}).invoke('sum', 2, 5, 10).should('be.greaterThan', 10).and('be.lessThan', 20)
+    
+  })
+})
