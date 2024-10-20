e91c21a45246cd138218a4f4b19e92730f83cf81
El Moussaoui <khalid.el-moussaoui@sogeti.com>
2024-10-21 00:40:05 +0200

    E2E Tests - UI Smoke Test

---
+++ cypress/e2e/TC_001_SmokeTest.cy.js
@@ -0,0 +1,12 @@
+
+
+describe('TC_001_SmokeTest', () => {
+  it('Should load home page', () => {
+    cy.visit('https://demoblaze.com/')
+    cy.get('.navbar-brand').should('be.visible')
+    cy.get('#navbarExample').should('be.visible')
+    cy.get('#cat').should('be.visible')
+    cy.get('#fotcont').should('be.visible')
+
+  })
+})
